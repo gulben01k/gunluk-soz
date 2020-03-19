93a30aa4f8e6086bdede0d02cc75033542df90cc
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2020-03-19 12:59:39 +0300

    added text update function

--- docs/assets/index/index.js
+++ docs/assets/index/index.js
@@ -1,1 +1,14 @@
 console.log("index.js is run");
+
+// yazı güncelleme fonksiyonları
+function guncelleTarih(a){
+    $("#tarih").text(a);
+}
+
+function guncelleYazi(a){
+    $("#yazi").text(a);
+}
+
+function guncelleYazar(a){
+    $("#yazar").text(a);
+}
